6f27943c93314fcd85da6f27f78de0dc843cc189
vintzvintz <git@vintz.fr>
2024-11-23 17:34:34 +0100

    parse JSON

--- mfmap/map.go
+++ mfmap/map.go
@@ -1,14 +1,36 @@
 package mfmap
 
 import (
+	"fmt"
 	"io"
 	"strings"
+	"encoding/json"
+	"golang.org/x/net/html"
 )
 
 type MfMap struct {
 	nom    string
 	parent *MfMap
-	html   []byte
+	data   []byte
+}
+
+type JsonData struct {
+	Path        PathType        `json:"path"`
+	MapLayersV2 MapLayersV2Type `json:"mf_map_layers_v2"`
+}
+
+type PathType struct {
+	BaseUrl    string `json:"baseUrl"`
+	ScriptPath string `json:"scriptPath"`
+}
+
+type MapLayersV2Type struct {
+	Nid         string `json:"nid"`
+	Name        string `json:"name"`
+	Path        string `json:"path"`
+	Taxonomy    string `json:"taxonomy"`
+	PathAssets  string `json:"path_assets"`
+	IdTechnique string `json:"field_id_technique"`
 }
 
 // accessor
@@ -29,17 +51,64 @@ func (m *MfMap) SetParent(parent *MfMap) {
 }
 
 func NewFrom(r io.Reader) (*MfMap, error) {
-	//return nil, fmt.Errorf("fail")
+	j, err := JsonFilter(r)
+	if err != nil {
+		return nil, err
+	}
+	data, err := io.ReadAll(j)
+	if err != nil {
+		return nil, err
+	}
+	return &MfMap{data: data}, nil
+}
+
+// isJsonTag detecte l'élement contenant les donnnées drupal
+// <script type="application/json" data-drupal-selector="drupal-settings-json">
+func isJsonTag(t html.Token) bool {
+	isScript := (t.Type == html.StartTagToken) && (t.Data == "script")
+	if !isScript {
+		return false
+	}
+	var hasAttrType, hasDrupalAttr bool
+	for _, a := range t.Attr {
+		switch a.Key {
+		case "type":
+			hasAttrType = (a.Val == "application/json")
+		case "data-drupal-selector":
+			hasDrupalAttr = (a.Val == "drupal-settings-json")
+		}
+	}
+	return isScript && hasAttrType && hasDrupalAttr
+}
+
+// JsonFilter extracts json data from an html page
+func JsonFilter(src io.Reader) (io.Reader, error) {
+	z := html.NewTokenizer(src)
+	var inJson bool
+loop:
+	for {
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken: // éventuellement io.EOF
+			break loop
+		case html.StartTagToken:
+			inJson = isJsonTag(z.Token())
+		case html.TextToken:
+			if inJson {
+				return strings.NewReader(z.Token().Data), nil
+			}
+		}
+	}
+	return nil, fmt.Errorf("données JSON non trouvées")
+}
+
+func JsonParser(r io.Reader) (*JsonData, error) {
+	var j JsonData
 	buf, err := io.ReadAll(r)
 	if err != nil {
 		return nil, err
 	}
-	return &MfMap{html: buf}, nil
-}
-
-func (m *MfMap) JsonContent() (io.Reader, error) {
-
-	// tokenizer
+	err = json.Unmarshal(buf, &j)
+	return &j, err
 	
-	return strings.NewReader("{le_vincent:1}"), nil
 }
--- mfmap/map_test.go
+++ mfmap/map_test.go
@@ -1,13 +1,17 @@
 package mfmap
 
 import (
+
 	"io"
 	"os"
+	"strings"
 	"testing"
 )
 
 const assets_path = "../test_data/"
 
+
+/*
 func TestNewMap(t *testing.T) {
 
 	const name = assets_path + "racine.html"
@@ -43,5 +47,83 @@ func TestNewMap(t *testing.T) {
 	txt, err := io.ReadAll(io.LimitReader(r,50))
 	if err == nil {
 		t.Logf( "JSON=%s", txt)
+	}
+}
+*/
+
+func TestJsonFilter(t *testing.T) {
+	const name = assets_path + "racine.html"
+	f, err := os.Open(name)
+	if err != nil {
+		t.Errorf("os.Open(%s) failed: %v", name, err)
+		return
+	}
+	defer f.Close()
+
+	// get json content
+	r, err := JsonFilter(f)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	data, err := io.ReadAll(io.LimitReader(r,50))
+	if err != nil {
+		t.Errorf("Failed to extract JSON data from %s", name)
+		return
+	}
+	t.Logf( "JSON=%s", data)
+}
+
+func TestJsonFilterFail(t *testing.T) {
+	r := strings.NewReader( `
+<html>
+<head>
+<title>JsonReader test</head>
+<body>
+<script> 
+	script element without attributes
+</script>
+<script src="/wesh.js" data-drupal-selector="drupal-settings-json">
+	wrong src attr, missing type attr
+/script>
+<script type="application/json" data-drupal-selector="drupal-settings-json">
+	wrong type attr
+/script>
+<script type="application/json" data-drupal-selector="wesh">
+	wrong drupal attr
+</script>
+<body>
+</html>`)
+	_, err := JsonFilter(r)
+	if err == nil {
+		t.Error("JsonReader did not returned error")
+		return
+	}
+}
+
+
+func TestJsonParsing(t *testing.T) {
+	const name = assets_path + "racine.json"
+	f, err := os.Open(name)
+	if err != nil {
+		t.Errorf("os.Open(%s) failed: %v", name, err)
+		return
+	}
+	defer f.Close()
+
+	j, err := JsonParser(f)
+	if err != nil {
+		t.Errorf("json.Unmarshal(%s) failed: %v", name, err)
+	}
+	t.Log(j)
+	// check few leafs
+	if j.Path.BaseUrl != "/" {
+		t.Errorf( "j.Path.BaseUrl=%v expected /", j.Path.BaseUrl)
+	}
+	if j.MapLayersV2.Taxonomy != "PAYS" {
+		t.Errorf( "j.MapLayersV2.Taxonomy=%v expected FRANCE", j.MapLayersV2.Taxonomy )
+	}
+	if j.MapLayersV2.IdTechnique != "PAYS007" {
+		t.Errorf( "j.MapLayersV2.IdTechnique=%v expected PAYS007", j.MapLayersV2.IdTechnique )
 	}
 }
