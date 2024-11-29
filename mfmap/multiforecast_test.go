a7033d3d4c3bc882d320e6a41265a9bf91e150fc
vintzvintz <git@vintz.fr>
2024-11-29 21:07:50 +0100

    test Feature.Type

--- mfmap/multiforecast_test.go
+++ mfmap/multiforecast_test.go
@@ -5,17 +5,31 @@ import (
 	"testing"
 )
 
-func TestParseMultiforecast(t *testing.T) {
-
+func validateMultiforecastFeature(t *testing.T, feat *feature) {
 	tests := map[string]struct {
 		want interface{}
 		got  func(*feature) interface{}
 	}{
 		"type": {
 			want: "Feature",
-			got:  func(mf *feature) interface{} { return mf.Type },
+			got:  func(f *feature) interface{} { return f.Type },
 		},
 	}
+	if feat == nil {
+		t.Fatal("no feature to validate")
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := test.got(feat)
+			want := test.want
+			if got != want {
+				t.Errorf("multiforecast feature %s error: got %s want %s", name, got, want )
+			}
+		})
+	}
+}
+
+func TestParseMultiforecast(t *testing.T) {
 
 	j := openFile(t, "multiforecast.json")
 	defer j.Close()
@@ -24,10 +38,8 @@ func TestParseMultiforecast(t *testing.T) {
 	if err != nil {
 		t.Fatal(fmt.Errorf("parseMultiforecast() error: %w", err))
 	}
-	if mf == nil {
-		t.Fatal("parseMultiforecast() returned nil data without error")
+	if len(mf) == 0 {
+		t.Fatal("parseMultiforecast() returned no data")
 	}
-
-	_ = tests
-	// TODO: check content
+	validateMultiforecastFeature(t, &mf[0])
 }
