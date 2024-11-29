d7f980e37fb62f951a1e7acde35f730f2e9b9458
vintzvintz <git@vintz.fr>
2024-11-29 20:52:28 +0100

    Parse featureCollection

--- mfmap/multiforecast_test.go
+++ mfmap/multiforecast_test.go
@@ -7,18 +7,27 @@ import (
 
 func TestParseMultiforecast(t *testing.T) {
 
+	tests := map[string]struct {
+		want interface{}
+		got  func(*feature) interface{}
+	}{
+		"type": {
+			want: "Feature",
+			got:  func(mf *feature) interface{} { return mf.Type },
+		},
+	}
+
 	j := openFile(t, "multiforecast.json")
 	defer j.Close()
-	m := parseHtml(t, fileHtmlRacine)
 
-	mf, err := m.parseMultiforecast(j)
-	if err != nil { 
-		t.Fatal( fmt.Errorf("parseMultiforecast() error: %w",err ) )
+	mf, err := parseMultiforecast(j)
+	if err != nil {
+		t.Fatal(fmt.Errorf("parseMultiforecast() error: %w", err))
 	}
 	if mf == nil {
 		t.Fatal("parseMultiforecast() returned nil data without error")
 	}
 
-	_ = mf
+	_ = tests
 	// TODO: check content
 }
--- mfmap/mutliforecast.go
+++ mfmap/mutliforecast.go
@@ -1,8 +1,48 @@
-
 package mfmap
 
-type MultiforecastData struct {
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"time"
+)
 
+type featureCollection struct {
+	Type     string    `json:"type"`
+	Features []feature `json:"features"`
 }
 
+type feature struct {
+	UpdateTime time.Time `json:"update_time"`
+	Type       string    `json:"type"`
+}
 
+type MultiforecastData []feature
+
+const featureCollectionStr = "FeatureCollection"
+
+func parseMultiforecast(r io.Reader) (MultiforecastData, error) {
+	fc, err := parseFeatureCollection(r)
+	if err != nil {
+		return nil, err
+	}
+	return fc.Features, nil
+}
+
+func parseFeatureCollection(r io.Reader) (*featureCollection, error) {
+
+	var fc featureCollection
+	j, err := io.ReadAll(r)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not read multiforecast data: %w", err)
+	}
+	err = json.Unmarshal(j, &fc)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling multiforecast data: %w", err)
+	}
+	if fc.Type != featureCollectionStr {
+		return nil, fmt.Errorf("featureCollection.Type got %s want %s", fc.Type, featureCollectionStr)
+	}
+	return &fc, nil
+}
