8f0ef851a3cf54990c59ee6ef51a700b10b6e9ec
vintzvintz <git@vintz.fr>
2024-12-08 15:24:58 +0100

    refactor

--- mfmap/build_test.go
+++ mfmap/build_test.go
@@ -1,7 +1,9 @@
 package mfmap
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestBuildJson(t *testing.T) {
@@ -24,4 +26,49 @@ func TestBuildJson(t *testing.T) {
 			Bbox Bbox
 		}
 	*/
+}
+
+func TestByEcheances(t *testing.T) {
+
+	mf := testParseMultiforecast(t, fileJsonMultiforecast)
+	prevs := mf.ByEcheance()
+	if len(prevs) == 0 {
+		t.Errorf("No forecast found in %s", fileJsonMultiforecast)
+	}
+}
+
+func TestGraphData(t *testing.T) {
+	mf := testParseMultiforecast(t, fileJsonMultiforecast)
+	prevs := mf.ByEcheance()
+
+	d := prevs.toChroniques()
+	if (d == nil) || (len(d) == 0) {
+		t.Errorf("Graphdata returned nothing from '%s'", fileJsonMultiforecast)
+	}
+}
+
+func TestEcheanceString(t *testing.T) {
+
+	m := morningStr
+	d, _ := time.Parse(time.RFC3339, "2024-12-02T15:51:12.000Z")
+	e := Echeance{MomentName(m), d}
+	want := fmt.Sprintf("%4d-%02d-%02d %s", d.Year(), d.Month(), d.Day(), m)
+
+	got := e.String()
+	if got != want {
+		t.Errorf("Echeance.String()='%s' want '%s'", got, want)
+	}
+}
+
+func TestFindDaily(t *testing.T) {
+	mf := testParseMultiforecast(t, fileJsonMultiforecast)
+	id := CodeInsee("440360") // "name": "Châteaubriant"
+	ech, err := time.Parse(time.RFC3339, "2024-12-02T00:00:00.000Z")
+	if err != nil {
+		t.Fatal(err)
+	}
+	d := mf.FindDaily(id, ech)
+	if d == nil {
+		t.Fatalf("FindDaily() did not found daily forecast for location '%s' at '%s'", id, ech)
+	}
 }
--- mfmap/multiforecast_test.go
+++ mfmap/multiforecast_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"regexp"
 	"testing"
-	"time"
 )
 
 const (
@@ -71,50 +70,5 @@ func TestPictoList(t *testing.T) {
 	pics := mf.pictoList()
 	if len(pics) == 0 {
 		t.Errorf("pictoList() returned nothing from '%s'", fileJsonMultiforecast)
-	}
-}
-
-func TestByEcheances(t *testing.T) {
-
-	mf := testParseMultiforecast(t, fileJsonMultiforecast)
-	prevs := mf.ByEcheance()
-	if len(prevs) == 0 {
-		t.Errorf("No forecast found in %s", fileJsonMultiforecast)
-	}
-}
-
-func TestGraphData(t *testing.T) {
-	mf := testParseMultiforecast(t, fileJsonMultiforecast)
-	prevs := mf.ByEcheance()
-
-	d := prevs.toChroniques()
-	if (d == nil) || (len(d) == 0) {
-		t.Errorf("Graphdata returned nothing from '%s'", fileJsonMultiforecast)
-	}
-}
-
-func TestEcheanceString(t *testing.T) {
-
-	m := morningStr
-	d, _ := time.Parse(time.RFC3339, "2024-12-02T15:51:12.000Z")
-	e := Echeance{MomentName(m), d}
-	want := fmt.Sprintf("%4d-%02d-%02d %s", d.Year(), d.Month(), d.Day(), m)
-
-	got := e.String()
-	if got != want {
-		t.Errorf("Echeance.String()='%s' want '%s'", got, want)
-	}
-}
-
-func TestFindDaily(t *testing.T) {
-	mf := testParseMultiforecast(t, fileJsonMultiforecast)
-	id := CodeInsee("440360") // "name": "Châteaubriant"
-	ech, err := time.Parse(time.RFC3339, "2024-12-02T00:00:00.000Z")
-	if err != nil {
-		t.Fatal(err)
-	}
-	d := mf.FindDaily(id, ech)
-	if d == nil {
-		t.Fatalf("FindDaily() did not found daily forecast for location '%s' at '%s'", id, ech)
 	}
 }
