161c2d1c8f15fdee3f73bd6fcd7525f8642eb691
vintzvintz <git@vintz.fr>
2024-12-31 21:26:29 +0100

    WIP updateMarkers()

--- static/js/components/LeVincent.js
+++ static/js/components/LeVincent.js
@@ -201,6 +201,9 @@ export const MapComponent = {
     // we need a reference in component instance to control tooltips and displayed data
     let lMap = null
 
+    // keep references to markers for update/deletion
+    let markers = []
+
     function initMap() {
       //  when timespan changes, components are cached/re-used by v-for algorithm
       // so just skip initMap because map and subzones do not change.
@@ -236,6 +239,11 @@ export const MapComponent = {
       lMap.setMinZoom(lMap.getBoundsZoom(bounds, true))
 
       drawSubzones()
+
+      // todo: add updated date in "attributions"
+
+      // todo : reactive markers on props.selections
+      updateMarkers()
     }
 
     function svgPath() {
@@ -276,10 +284,123 @@ export const MapComponent = {
       })
     }
 
+    function updateMarkers() {
+      let pois = props.prev && props.prev.prevs
+      console.log(['before updateMarkers()', markers])
+      if (pois) {
+          removeMarkers();   // todo : inline
+          pois.forEach( createMarker );
+
+        // TODO
+        // this.updateControl.setPrefix ("Màj : "+this.get_prevs.updated );
+      }
+      console.log(['after updateMarkers()', markers])
+    }
+
+    function removeMarkers() {
+      while ( markers.length > 0 ) {
+        lMap.removeLayer(markers.pop());
+      }
+    }
+
+    function createMarker(poi, idx, all_prevs) {
+
+      // local aliases
+      const prev = poi.prev
+      const daily = poi.daily
+
+      // accumulate marker data for current poi
+      const mark = {}
+
+      // default marker
+      mark.disabled = false;
+      mark.icon = prev.weather_icon;
+      // TODO: fallback on daily_weather_icon if weather_icon == null
+      mark.txt = null
+
+      if ( prev.T !== null ) {
+        // donnée court-terme en priorité si disponibles
+        mark.T = Math.round(prev.T)
+        mark.txt = mark.T + "°"
+      } else if (daily.T_min !== null && daily.T_max !==null) {
+        // donnée long-terme (dailies) 
+        mark.txt = ' <span class="tmin">' + Math.round(daily.T_min) + '°</span>' +
+                  '/<span class="tmax">' + Math.round(daily.T_max) + '°</span>'
+      } else {
+          // pas de marker si temperature indisponible
+          mark.disabled = true
+      }
+
+      mark.coords = poi.coords
+      mark.titre = poi.titre
+      // TODO: fallback on daily_weather_desc if weather_desc == null
+      mark.desc = prev.weather_description
+      mark.Tmin = Math.round(prev.T_min)
+      mark.Tmax = Math.round(prev.T_max)
+
+      addMarker( mark )
+    }
+
+
+    function addMarker(mark) {
+      if( mark.disabled) {
+        return 
+      }
+//      console.log(["addMarker()", mark])
+      let icon_width = 40
+      if ( props.selections.activeWeather == 'vent') {
+        icon_width = 25    // icones du vent plus petites
+      }
+
+      // style pour le texte température min/max (qui contient un slash)
+      let icon_text_style = ""
+      /*
+      if( ("string" == typeof e.icon_text || e.icon_text instanceof String) && 
+              e.icon_text.indexOf("/") > -1 ) {
+          (icon_text_style = "font-size: 12px;");  */
+
+      let elt_a = '<a>'+
+      '<img src="/pictos/' + mark.icon + '" ' +
+      'alt="' + mark.desc + '" ' +
+      'title="' + mark.titre + '" ' +
+      'class="icon shape-weather" ' +
+      'style="width: ' + icon_width + 'px"/>'
+
+      if( mark.txt && "" != mark.txt && "NaN°" != mark.txt ) {
+        elt_a += '<span class="icon_text" style="' + icon_text_style + '">' +
+                  mark.txt + '</span>'
+      }
+      elt_a += "</a>"
+
+      let mark_opts = {
+        icon : L.divIcon({
+          html: elt_a,
+          className: "iconMap-1",
+          iconSize: [icon_width, icon_width],
+          iconAnchor: [icon_width / 2, icon_width / 2]
+        })
+      }
+
+      // GeoJSON order : lng,lat
+      // Leaflet API order : lat,lng
+      //let coords = L.LatLng( mark.coords[1], mark.coords[0])
+      let lMark = L.marker([mark.coords[1], mark.coords[0]], mark_opts).addTo(lMap);
+
+      // keep a reference for marker deletion
+      markers.push(lMark)
+    }
+/*
+    function markerPosition(coords) {
+      let bbox = props.data.bbox
+      let pos_h = (coords[0] > (bbox.o + bbox.e)/2 ) ? 'Right' : 'Left'
+      let pos_v = (coords[1] > (bbox.n + bbox.s)/2 ) ? 'Top' : 'Bottom'
+      return pos_v+' '+pos_h
+    }
+*/
+
     onMounted(initMap)
     return { mapTitle, mapId }
   },
-
 
   template: /*html*/`
 <div class="map_grid_item">
