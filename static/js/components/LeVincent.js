15d10fdf48d838192ac15b3a01748647d28991b4
vintzvintz <git@vintz.fr>
2025-01-02 12:06:33 +0100

    css flex positionning

--- static/js/components/LeVincent.js
+++ static/js/components/LeVincent.js
@@ -126,42 +126,30 @@ export const MapGridComponent = {
 
   setup(props) {
 
-    const displayedJours = (() => {
+    function displayedJours() {
       //console.log( "displayedJours() typeof props.data.prevs is " + typeof props.data.prevs )
-
       const ret = []
-      if ( typeof props.data.prevs === 'undefined') {
-        return ret
-      }
-      for (var i = -1; i < 3; i++) {
-        if ( Object.hasOwn(props.data.prevs, i)) {
-          ret.push(props.data.prevs[i])
+      if (typeof props.data.prevs !== 'undefined') {
+        for (var i = -1; i < 2; i++) {
+          if (Object.hasOwn(props.data.prevs, i)) {
+            ret.push(props.data.prevs[i])
+          }
         }
       }
-/*    for (let jour in props.data.prevs) {
-        let n = parseInt(jour)
-        //if (n < 3 && n >= 0) {
-          if (n < 3) {
-          ret.push(props.data.prevs[jour])
-        }
-      }*/
       return ret
-    })
+    }
 
     return { displayedJours }
   },
 
   template: /*html*/`
-<div class="map_grid_container">
-  <div>
-    <p>  before MapRowComponent </p> 
+<div class="maps_grid">
     <MapRowComponent
     v-for="(jour, idx) in displayedJours()"
     :key="idx"
     :prevsDuJour="jour"
     :data="data"
     :selections="selections"/>
-  </div>
 </div>`
 }
 
@@ -178,13 +166,13 @@ export const MapRowComponent = {
   },
 
   template: /*html*/`
-  <p> ... inside MapRowComponent ... </p>
+ <div class="maps-row">
   <MapComponent v-for="(prev, idx) in prevsDuJour"
   :key="idx"
   :prev="prev"
   :data="data"
-  :selections="selections">
-</MapComponent>`
+  :selections="selections"/>
+</div>`
 }
 
 
@@ -207,7 +195,7 @@ export const MapComponent = {
 
     let _map_id = 0
     const mapId = function () {
-      if( _map_id == 0 ) {
+      if (_map_id == 0) {
         _map_id = nextMapId()
       }
       return String(_map_id)
@@ -215,7 +203,7 @@ export const MapComponent = {
 
     const mapTitle = function () {
       let weather = (typeof props.selections != null) ? props.selections.activeWeather : ""
-      let moment = (props.prev != null)  ? props.prev.echeance : ""
+      let moment = (props.prev != null) ? props.prev.echeance : ""
       return moment + ' - ' + weather
     }
 
@@ -294,13 +282,13 @@ export const MapComponent = {
               this.setStyle({ color: "#FFF", fillColor: "transparent" })
               layer.openPopup()
             }),
-            layer.on("mouseout", function () {
-              this.setStyle({ color: "transparent", fillColor: "transparent" })
-              layer.closePopup()
-            }),
-            layer.on("click", function () {
-              window.location = path
-            })
+              layer.on("mouseout", function () {
+                this.setStyle({ color: "transparent", fillColor: "transparent" })
+                layer.closePopup()
+              }),
+              layer.on("click", function () {
+                window.location = path
+              })
           }
         }
         ).addTo(lMap);
@@ -310,8 +298,8 @@ export const MapComponent = {
     function updateMarkers() {
       let pois = props.prev && props.prev.prevs
       if (pois) {
-          removeMarkers();   // todo : inline
-          pois.forEach( createMarker );
+        removeMarkers();   // todo : inline
+        pois.forEach(createMarker);
 
         // TODO
         // this.updateControl.setPrefix ("Màj : "+this.get_prevs.updated );
@@ -319,7 +307,7 @@ export const MapComponent = {
     }
 
     function removeMarkers() {
-      while ( markers.length > 0 ) {
+      while (markers.length > 0) {
         lMap.removeLayer(markers.pop());
       }
     }
@@ -339,17 +327,17 @@ export const MapComponent = {
       // TODO: fallback on daily_weather_icon if weather_icon == null
       mark.txt = null
 
-      if ( prev.T !== null ) {
+      if (prev.T !== null) {
         // donnée court-terme en priorité si disponibles
         mark.T = Math.round(prev.T)
         mark.txt = mark.T + "°"
-      } else if (daily.T_min !== null && daily.T_max !==null) {
+      } else if (daily.T_min !== null && daily.T_max !== null) {
         // donnée long-terme (dailies) 
         mark.txt = ' <span class="tmin">' + Math.round(daily.T_min) + '°</span>' +
-                  '/<span class="tmax">' + Math.round(daily.T_max) + '°</span>'
+          '/<span class="tmax">' + Math.round(daily.T_max) + '°</span>'
       } else {
-          // pas de marker si temperature indisponible
-          mark.disabled = true
+        // pas de marker si temperature indisponible
+        mark.disabled = true
       }
 
       mark.coords = poi.coords
@@ -359,17 +347,17 @@ export const MapComponent = {
       mark.Tmin = Math.round(prev.T_min)
       mark.Tmax = Math.round(prev.T_max)
 
-      addMarker( mark )
+      addMarker(mark)
     }
 
 
     function addMarker(mark) {
-      if( mark.disabled) {
-        return 
+      if (mark.disabled) {
+        return
       }
-//      console.log(["addMarker()", mark])
+      //      console.log(["addMarker()", mark])
       let icon_width = 40
-      if ( props.selections.activeWeather == 'vent') {
+      if (props.selections.activeWeather == 'vent') {
         icon_width = 25    // icones du vent plus petites
       }
 
@@ -380,21 +368,21 @@ export const MapComponent = {
               e.icon_text.indexOf("/") > -1 ) {
           (icon_text_style = "font-size: 12px;");  */
 
-      let elt_a = '<a>'+
-      '<img src="/pictos/' + mark.icon + '" ' +
-      'alt="' + mark.desc + '" ' +
-      'title="' + mark.titre + '" ' +
-      'class="icon shape-weather" ' +
-      'style="width: ' + icon_width + 'px"/>'
+      let elt_a = '<a>' +
+        '<img src="/pictos/' + mark.icon + '" ' +
+        'alt="' + mark.desc + '" ' +
+        'title="' + mark.titre + '" ' +
+        'class="icon shape-weather" ' +
+        'style="width: ' + icon_width + 'px"/>'
 
-      if( mark.txt && "" != mark.txt && "NaN°" != mark.txt ) {
+      if (mark.txt && "" != mark.txt && "NaN°" != mark.txt) {
         elt_a += '<span class="icon_text" style="' + icon_text_style + '">' +
-                  mark.txt + '</span>'
+          mark.txt + '</span>'
       }
       elt_a += "</a>"
 
       let mark_opts = {
-        icon : L.divIcon({
+        icon: L.divIcon({
           html: elt_a,
           className: "iconMap-1",
           iconSize: [icon_width, icon_width],
@@ -410,14 +398,14 @@ export const MapComponent = {
       // keep a reference for marker deletion
       markers.push(lMark)
     }
-/*
-    function markerPosition(coords) {
-      let bbox = props.data.bbox
-      let pos_h = (coords[0] > (bbox.o + bbox.e)/2 ) ? 'Right' : 'Left'
-      let pos_v = (coords[1] > (bbox.n + bbox.s)/2 ) ? 'Top' : 'Bottom'
-      return pos_v+' '+pos_h
-    }
-*/
+    /*
+        function markerPosition(coords) {
+          let bbox = props.data.bbox
+          let pos_h = (coords[0] > (bbox.o + bbox.e)/2 ) ? 'Right' : 'Left'
+          let pos_v = (coords[1] > (bbox.n + bbox.s)/2 ) ? 'Top' : 'Bottom'
+          return pos_v+' '+pos_h
+        }
+    */
 
     onMounted(initMap)
     return { mapTitle, mapId }
