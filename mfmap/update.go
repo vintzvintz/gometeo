8c56bac9908afcab7ada0883aa72d88b5e367804
vintzvintz <git@vintz.fr>
2025-01-12 11:14:38 +0100

    ShouldUpdate()

--- mfmap/update.go
+++ mfmap/update.go
@@ -6,17 +6,41 @@ import (
 )
 
 type Stats struct {
-	lastUpdate time.Time
-	lastHit    time.Time
-	hitCount   atomic.Int64
+	lastUpdate atomic.Int64 // unix time, casted to time.Time by accessors
+	lastHit    atomic.Int64 // unix time, casted to time.Time by accessors
+	hitCount   atomic.Int64 // simple counter
 }
 
-func (m *MfMap) Hit() {
-	_ = m.stats.lastHit
-	_ = m.stats.lastUpdate
+const (
+	fastModeDuration = 3 * 24 * time.Hour // duration of fast update after last hit
+	fastModeMaxAge   = 30 * time.Minute
+	slowModeMaxAge   = 4 * time.Hour
+)
+
+func (m *MfMap) LogUpdate() {
+	now := time.Now().Unix()
+	m.stats.lastUpdate.Store(now)
+}
+
+func (m *MfMap) LogHit() {
+	now := time.Now().Unix()
+	m.stats.lastHit.Store(now)
 	m.stats.hitCount.Add(1)
+}
+
+func (m *MfMap) LastHit() time.Time {
+	return time.Unix(m.stats.lastHit.Load(), 0)
 }
 
 func (m *MfMap) HitCount() int64 {
 	return m.stats.hitCount.Load()
+}
+
+func (m *MfMap) ShouldUpdate() bool {
+	updateAge := time.Since(time.Unix(m.stats.lastUpdate.Load(), 0))
+	hitAge := time.Since(time.Unix(m.stats.lastHit.Load(), 0))
+	if hitAge < fastModeDuration {
+		return updateAge > fastModeMaxAge
+	}
+	return updateAge > slowModeMaxAge
 }
--- mfmap/update_test.go
+++ mfmap/update_test.go
@@ -3,6 +3,7 @@ package mfmap
 import (
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestHitCountRace(t *testing.T) {
@@ -11,13 +12,13 @@ func TestHitCountRace(t *testing.T) {
 	start := make(chan struct{})
 
 	// prepare some goroutines
-	const n = 10000
+	const n = 50000
 	for i := 0; i < n; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			<-start // wait for start signal
-			m.Hit()
+			m.LogHit()
 		}()
 	}
 	// start all goroutines for massive hitting !
@@ -26,6 +27,71 @@ func TestHitCountRace(t *testing.T) {
 	wg.Wait()
 
 	if m.HitCount() != n {
-		t.Fatalf("concurrency issues...  got %d hits, want %d", m.HitCount(), n)
+		t.Errorf("concurrency issues...  got %d hits, want %d", m.HitCount(), n)
+	}
+	if time.Since(m.LastHit()).Abs() > time.Second {
+		t.Errorf("last hit time not properly recorded")
+	}
+}
+
+func TestLogUpdate(t *testing.T) {
+	m := MfMap{}
+	before := m.ShouldUpdate()
+	if !before {
+		t.Error("ShouldUpdate() on zero-valued map must be true")
+	}
+	m.LogUpdate()
+	after := m.ShouldUpdate()
+	if after {
+		t.Error("ShouldUpdate() on just-updated map must be false")
+	}
+}
+
+func TestShouldUpdate(t *testing.T) {
+
+	var tests = map[string]struct {
+		update int64
+		hit    int64
+		want   bool
+	}{
+		"zero": {
+			update: 0,
+			want:   true,
+		},
+		"now": {
+			update: time.Now().Unix(),
+			want:   false,
+		},
+		"fast_true": {
+			hit:    time.Now().Add(-fastModeDuration).Unix() + 10, // last hit 10 sec after fastmode cutoff
+			update: time.Now().Add(-fastModeMaxAge).Unix() - 30,   // last update 30 sec before cutoff
+			want:   true,
+		},
+		"fast_false": {
+			hit:    time.Now().Add(-fastModeDuration).Unix() + 10, // last hit 10 sec after fastmode cutoff
+			update: time.Now().Add(-fastModeMaxAge).Unix() + 30,   // last update 30 sec after cutoff
+			want:   false,
+		},
+		"slow_true": {
+			hit:    time.Now().Add(-fastModeDuration).Unix() - 10, // last hit 10 sec before fastmode cutoff
+			update: time.Now().Add(-slowModeMaxAge).Unix() - 30,   // last update 30 sec before cutoff
+			want:   true,
+		},
+		"slow_false": {
+			hit:    time.Now().Add(-fastModeDuration).Unix() - 10, // last hit 10 sec before fastmode cutoff
+			update: time.Now().Add(-slowModeMaxAge).Unix() + 30,   // last update 30 sec after cutoff
+			want:   false,
+		},
+	}
+	m := MfMap{}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			m.stats.lastUpdate.Store(test.update)
+			m.stats.lastHit.Store(test.hit)
+			got := m.ShouldUpdate()
+			if got != test.want {
+				t.Errorf("ShouldUpdate() got %v, want %v", got, test.want)
+			}
+		})
 	}
 }
